c09b176f1bf9e605cde0c98d7f2e318c1648e7b9
miranda <mirandachiu13@gmail.com>
2016-12-02 19:48:23 -0500

    comment editing now works c:

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
-  #rails make table names automaatically plural - that's stupid we gun change dat 
+  #rails make table names automaatically plural - that's stupid we gun change dat
   self.table_name = "comment"
+
+  def author_name
+    #IN SQL GET AUTHOR NAME
+        author = User.find_by_sql("SELECT * FROM user WHERE id = #{self.author};").first
+        author.name
+  end
 end
