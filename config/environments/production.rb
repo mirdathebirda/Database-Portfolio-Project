89ebddb50b8fb39364c0b5407bce4e6a1acfb64b
miranda <mirandachiu13@gmail.com>
2016-12-04 15:24:15 -0500

    testing whether image will show up on heroku now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,3 +84,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+config.serve_static_assets = true
+config.assets.compile = true
