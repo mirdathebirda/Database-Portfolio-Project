947a9bb805c76b8459e5413921b8ea1bb76e20d8
miranda <mirandachiu13@gmail.com>
2016-12-04 15:26:07 -0500

    welp that didn't work lets try something new

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,3 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
-
-config.serve_static_assets = true
-config.assets.compile = true
