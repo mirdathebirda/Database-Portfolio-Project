c8cac8515b3d884ad24729367eddde6d4f51441f
lixiang23 <li.xiang5@husky.neu.edu>
2016-12-05 15:11:19 -0500

    still need to fix margins

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,8 @@ class CategoriesController < ApplicationController
   def create
     cols = post_category_params.keys.join(", ")
     vals = post_category_params.values.map{|val| %Q(#{PostCategory.sanitize(val)})}.join(", ")
-    Category.connection.execute("INSERT INTO category(#{cols}) VALUES (#{vals});")
+    Category.connection.execute("INSERT INTO category(#{cols}) SELECT * FROM (SELECT #{vals}) AS tmp WHERE NOT EXISTS (
+    SELECT name FROM category WHERE name = #{vals});")
     category = Category.find_by_sql("SELECT * FROM category WHERE name = #{vals};").first
     PostCategory.connection.execute("INSERT INTO post_category(post, category) VALUES (#{params[:post_id]}, #{category.id})")
     redirect_to "/blogs/#{params[:blog_id]}/posts/#{params[:post_id]}"
