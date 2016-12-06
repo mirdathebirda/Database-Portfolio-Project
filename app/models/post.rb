da19207523199732a7fe590cdb6fad7ccba23685
Miranda Chiu <mirdathebirda@users.noreply.github.com>
2016-12-06 23:40:42 +0000

    added author and dates to posts and blogs (#16)
    * author shows up on post
    
    * author name shown in blog page now
    
    * blogs have authors now
    
    * author of posts show up on blog page
    
    * a post has a date
    
    * post author and dates on blog

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,4 +18,16 @@ class Post < ApplicationRecord
   def starred_by(user)
     !Star.find_by_sql("SELECT * FROM star WHERE post = #{self.id} AND user = #{user.id} AND starred = true;").empty?
   end
+
+  def author_name
+        author = User.find_by_sql("SELECT * FROM user WHERE id = #{self.author};").first
+        author.name
+  end
+
+  def format_date
+    single_post =  User.find_by_sql("SELECT * FROM post WHERE id = #{self.id};").first
+    require 'date'
+    d = Date.parse(single_post.date.to_s)
+    d.strftime("%v")
+  end
 end
