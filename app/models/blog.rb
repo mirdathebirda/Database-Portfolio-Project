06f45199a1f47207a41d10b9f3a6a1de5d926154
miranda <mirandachiu13@gmail.com>
2016-12-06 18:11:54 -0500

    blogs have authors now

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,11 @@
 class Blog < ApplicationRecord
   self.table_name = "blog"
 
+  def author_name
+        author = User.find_by_sql("SELECT * FROM user WHERE id = #{self.owner};").first
+        author.name
+  end
+
   def posts
     Post.find_by_sql "SELECT * FROM post WHERE blog = #{self.id};"
   end
