bbd7084f8b487bcfe833cf4964ad3adcfda51db8
Michael <michaelanhu@gmail.com>
2016-12-06 14:11:26 -0500

    cleardb doesn't support triggers

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -3,17 +3,29 @@ class StarsController < ApplicationController
   before_filter :authorize
 
   def star
-    star = Star.find_by_sql("SELECT * FROM star WHERE user = #{current_user.id} AND post = #{params[:post_id]};")
-    if star.empty?
+    stars = Star.find_by_sql("SELECT * FROM star WHERE user = #{current_user.id} AND post = #{params[:post_id]};")
+    if stars.empty?
       Star.connection.execute("INSERT INTO star (user, post) VALUES (#{current_user.id}, #{params[:post_id]});")
     else
-      Star.connection.execute("UPDATE star SET starred=true WHERE id = #{star.first.id};")
+      Star.connection.execute("UPDATE star SET starred=true WHERE id = #{stars.first.id};")
     end
+
+    # See migrations, this should have been a trigger if clearDB supported them
+    Star.connection.execute "UPDATE post SET star_count = (SELECT COUNT(*)
+              FROM star
+              WHERE star.post = post.id
+              AND starred = true) WHERE id = #{params[:post_id].to_i};"
     redirect_to :back
   end
 
   def unstar
     Star.connection.execute("UPDATE star SET starred=false WHERE user = #{current_user.id} AND post = #{params[:post_id]};")
+    
+    # See migrations, this should have been a trigger if clearDB supported them
+    Star.connection.execute "UPDATE post SET star_count = (SELECT COUNT(*)
+              FROM star
+              WHERE star.post = post.id
+              AND starred = true) WHERE id = #{params[:post_id].to_i};"
     redirect_to :back
   end
 end
--- db/migrate/20161206182118_add_star_count_trigger.rb
+++ db/migrate/20161206182118_add_star_count_trigger.rb
@@ -1,34 +1,35 @@
 class AddStarCountTrigger < ActiveRecord::Migration[5.0]
   def up
     execute "ALTER TABLE post ADD COLUMN star_count INT NOT NULL DEFAULT 0;"
-    execute "CREATE TRIGGER star_count_update
-              AFTER UPDATE ON star FOR EACH ROW
-              BEGIN
-                DECLARE stars INT;
+    ## No Trigger support on free version of ClearDB
+    # execute "CREATE TRIGGER star_count_update
+    #           AFTER UPDATE ON star FOR EACH ROW
+    #           BEGIN
+    #             DECLARE stars INT;
 
-                SELECT COUNT(*) INTO stars
-                  FROM star
-                  WHERE star.post = NEW.post
-                  AND starred = true;
+    #             SELECT COUNT(*) INTO stars
+    #               FROM star
+    #               WHERE star.post = NEW.post
+    #               AND starred = true;
 
-                UPDATE post
-                SET post.star_count = stars
-                WHERE NEW.post = post.id;
-              END"
-    execute "CREATE TRIGGER star_count_insert
-              AFTER INSERT ON star FOR EACH ROW
-              BEGIN
-                DECLARE stars INT;
+    #             UPDATE post
+    #             SET post.star_count = stars
+    #             WHERE NEW.post = post.id;
+    #           END"
+    # execute "CREATE TRIGGER star_count_insert
+    #           AFTER INSERT ON star FOR EACH ROW
+    #           BEGIN
+    #             DECLARE stars INT;
 
-                SELECT COUNT(*) INTO stars
-                  FROM star
-                  WHERE star.post = NEW.post
-                  AND starred = true;
+    #             SELECT COUNT(*) INTO stars
+    #               FROM star
+    #               WHERE star.post = NEW.post
+    #               AND starred = true;
 
-                UPDATE post
-                SET post.star_count = stars
-                WHERE NEW.post = post.id;
-              END"
+    #             UPDATE post
+    #             SET post.star_count = stars
+    #             WHERE NEW.post = post.id;
+    #           END"
     execute "UPDATE post SET star_count = (SELECT COUNT(*)
               FROM star
               WHERE star.post = post.id
@@ -36,8 +37,9 @@ class AddStarCountTrigger < ActiveRecord::Migration[5.0]
   end
 
   def down
-    execute "DROP TRIGGER star_count_insert;"
-    execute "DROP TRIGGER star_count_update;"
+    ## No Trigger support on free version of ClearDB
+    # execute "DROP TRIGGER star_count_insert;"
+    # execute "DROP TRIGGER star_count_update;"
     execute "ALTER TABLE post DROP COLUMN star_count;"
   end
 end
