8c1ba7bc668110d58639361414afe24dfab97992
Michael <michaelanhu@gmail.com>
2016-12-07 03:15:30 -0500

    fix search

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,17 +4,17 @@ class Post < ApplicationRecord
   def self.search(search, blog, page_size, page)
     # searching categories
     category_post_ids = PostCategory.find_by_sql("SELECT * FROM post_category INNER JOIN category ON post_category.category = category.id
-      WHERE category.name LIKE '%#{search}%' ORDER BY post_category.post DESC;").map(&:post)
+      WHERE category.name LIKE '%#{search}%' ORDER BY post_category.post DESC;").map(&:post).uniq
     if category_post_ids.empty?
       category_posts = []
     else
-      category_posts = Post.find_by_sql("SELECT * FROM post WHERE id = (#{category_post_ids.to_s[1..-2]});")
+      category_posts = Post.find_by_sql("SELECT * FROM post WHERE id IN (#{category_post_ids.to_s[1..-2]});")
     end
 
     # search authors, titles, text
     posts = Post.find_by_sql("SELECT * FROM post INNER JOIN user ON post.author = user.id WHERE blog = #{blog} AND (title LIKE '%#{search}%' OR body LIKE '%#{search}%' OR user.name LIKE '%#{search}%');")
     puts "#{category_posts.map(&:id)} blahh #{posts.map(&:id)}"
-    results = category_posts + posts
+    results = (category_posts + posts).uniq
     results = results[((page - 1) * page_size)..-1][0..page_size]
   end
 
